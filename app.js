e398de19ab841fc80cd401e3de85bf1da2ab9edc
chiaraintech <chiaraintech@gmail.com>
2021-04-16 13:17:26 +0100

    Added Login and Register routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
 const app = express();
+const PORT = process.env.PORT  || 5000;
 
-const PORT = process.env.PORT  || 5000
+//ROUTES
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`))
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
