883d55b8278b03674fdbea7c4efdeb1edb22d85a
chiaraintech <chiaraintech@gmail.com>
2021-04-16 13:50:17 +0100

    Rendering a view instead of a string.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Welcome to the Browser.'))
+router.get('/', (req, res) => res.render('welcome'))
 module.exports = router;
