e15c6b87192f81ca792a161882cf3be0a538ac2b
chiaraintech <chiaraintech@gmail.com>
2021-04-16 14:26:06 +0100

    Added registration handling for POST method.

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,42 @@ const express = require('express');
 const router = express.Router();
 
 //LOGIN PAGE
-router.get('/login', (req, res) => res.render('login'))
-module.exports = router;
+router.get('/login', (res) => res.render('login'));
 
 //REGISTER PAGE
-router.get('/register', (req, res) => res.render('register'))
+router.get('/register', (res) => res.render('register'));
+
+//REGISTER HANDLE
+router.post('/register', (req, res) => {
+    const {name, email, password, password2} = req.body;
+    let errors = [];
+
+    //Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: "Please fill in all fields." });
+    }
+
+    //Check passwords match
+    if (password != password2) {
+        errors.push({ msg: "Please check your emails match."});
+    }
+
+    //Check password length
+    if (password.length <= 6) {
+        errors.push({ msg: "Please ensure that passwords have more than 6 characters long."});
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        res.send('pass');
+    }
+})
+
 module.exports = router;
